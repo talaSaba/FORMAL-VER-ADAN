a29c8d8a6d9b28138e93c239eb0e5b0b81897a36
talaSaba <131599230+talaSaba@users.noreply.github.com>
2025-12-13 12:06:38 +0200

    Add files via upload

--- assignment02/properties.sv
+++ assignment02/properties.sv
@@ -13,11 +13,18 @@ module properties #(parameter FLOORS = 5)
 );
  //###############################################################
 //aux for q1.2
-logic first_c;
-initial first_c=1'b1;
-always_ff @(posedge clk)
-    first_c <= 1'b0;
-//###############################################################
+logic after_reset_1cycle;
+
+always_ff @(posedge clk) begin
+  if (rst) after_reset_1cycle <= 1'b1;
+  else after_reset_1cycle <= 1'b0;
+end
+
+property prop_Q1b;
+  @(posedge clk) after_reset_1cycle |-> $onehot(currentFloor);
+endproperty
+assume_Q1b: assume property (prop_Q1b);
+
 
 //###############################################################
 //what did we do : we initialised a new register called first_c and 
@@ -44,26 +51,6 @@ property prop_Q1a;
 endproperty
 assume_Q1a: assume property (prop_Q1a);
 
-// QUESTION 1(b): Assume we start from some (specific, single) floor.
-// NOTE: You are required to use auxiliary code for this question.
-// QUESTION 1(b): Assume we start from some (specific, single) floor.
-// NOTE: You are required to use auxiliary code for this question.
-logic [FLOORS-1:0] currentFloor_onehot_check;
-
-// Auxiliary code to check that currentFloor has exactly one bit set
-//always_comb begin
-//    currentFloor_onehot_check = '0;
-//    for (int i = 0; i < FLOORS; i++) begin
-//        if (currentFloor[i]) begin
-//            currentFloor_onehot_check[i] = 1'b1;
-//        end
-//    end
-//end
-
-property prop_Q1b;
-    @(posedge clk) first_c&&rst |-> ($onehot(currentFloor));
-endproperty
-assume_Q1b: assume property (prop_Q1b);
 
 // QUESTION 2: Check we don't hit the basement.
 property prop_Q2;
@@ -86,55 +73,64 @@ assert_Q4: assert property (prop_Q4);
 // QUESTION 5:
 property prop_Q5;
     @(posedge clk) 
-    (requestFloor[0]) |->  ##[1:$] (currentFloor[0] && (engineOp == STOP) && (doorsOp == OPEN));
-endproperty // EDIT THIS LINE
+    (requestFloor[0]) |-> ##[1:$](currentFloor[0]&&(engineOp == STOP)&&(doorsOp == OPEN));
+endproperty 
 assert_Q5: assert property (prop_Q5);
 
 // QUESTION 6:
 property prop_Q6;
-    @(posedge clk) (floorLight[0]&&!currentFloor[0])[*40] ; // EDIT THIS LINE
+    @(posedge clk)(floorLight[0]&&!currentFloor[0])[*40] ;// EDIT THIS LINE
 endproperty
 cover_Q6: cover property (prop_Q6);
 
 // QUESTION 7:
 property prop_Q7;
-    @(posedge clk)   (currentFloor[0] && engineOp==STOP && doorsOp==OPEN && direction==UP)
-  |->
-  (direction == UP) throughout
-  (
-    // Repeat FLOORS-1 times:
-    (
-      // stutter allowed: same floor, stopped & open
-      ##[0:$] ($stable(currentFloor) && engineOp==STOP && doorsOp==OPEN)
-      // move up exactly one floor (never down, no skipping)
-      ##1 (currentFloor == ($past(currentFloor) << 1))
-      // must stop and open after arriving
-      ##[0:$] ($stable(currentFloor) && engineOp==STOP && doorsOp==OPEN)
-    )[*(FLOORS-1)]
-    // finally, reach the top floor with doors open and stopped
-    ##[0:$] (currentFloor[FLOORS-1] && engineOp==STOP && doorsOp==OPEN)
-  );
+    @(posedge clk)(!(engineOp==GO && direction==DOWN)) 
+    throughout(
+      ##[0:$]( $onehot(currentFloor) && currentFloor[0] &&engineOp==STOP && doorsOp==OPEN )
+##[0:$](##[0:$](engineOp==GO && direction==UP)##1($onehot(currentFloor)&&engineOp==STOP&&doorsOp==OPEN &&(currentFloor==$past(currentFloor<<1))))[*(FLOORS-1)]
+##0($onehot(currentFloor) && currentFloor[FLOORS-1]&&engineOp==STOP && doorsOp==OPEN ));
 endproperty
 cover_Q7: cover property (prop_Q7);
 
 // QUESTION 8:
 property prop_Q8;
-    @(posedge clk)currentFloor[0] ##1 currentFloor[1];
-    
-    //(!(direction==UP))throughout( (currentFloor[FLOORS-1]&&engineOp==STOP&&doorsOp==OPEN) ##[1:$]
-    //(currentFloor[0]&&engineOp==STOP&&doorsOp==OPEN)
-   // ) ; // EDIT THIS LINE
+    @(posedge clk)(!(engineOp==GO && direction==UP))throughout(##[0:$]($onehot(currentFloor)&&currentFloor[FLOORS-1]&&engineOp==STOP&&doorsOp==OPEN)##[0:$](##[0:$] (engineOp==GO && direction==DOWN)
+##1( $onehot(currentFloor)&&engineOp==STOP && doorsOp==OPEN&&(currentFloor == $past(currentFloor >> 1))))[* (FLOORS-1)]
+##0($onehot(currentFloor) && currentFloor[0]&&engineOp==STOP && doorsOp==OPEN));
 endproperty
 cover_Q8: cover property (prop_Q8);
 
 // QUESTION 9:
 property prop_Q9;
-    @(posedge clk)((!(direction==DOWN))throughout( (currentFloor[0]&&engineOp==STOP&&doorsOp==OPEN) ##[1:$]
-    (currentFloor[FLOORS-1]&&engineOp==STOP&&doorsOp==OPEN)
-    )) ##0 ((!(direction==UP))throughout( (currentFloor[FLOORS-1]&&engineOp==STOP&&doorsOp==OPEN) ##[1:$]
-    (currentFloor[0]&&engineOp==STOP&&doorsOp==OPEN)
-    ))[*10]
-    ; // EDIT THIS LINE
+    @(posedge clk)
+    
+    //Q7****************************************************************
+    ((!(engineOp==GO && direction==DOWN)) throughout(##[0:$] ($onehot(currentFloor)&&currentFloor[0]&&engineOp==STOP && doorsOp==OPEN )
+##[0:$](##[0:$] (engineOp==GO && direction==UP)##1($onehot(currentFloor)&&engineOp==STOP&&doorsOp==OPEN&&(currentFloor==$past(currentFloor<< 1))))[*(FLOORS-1)]
+##0($onehot(currentFloor)&&currentFloor[FLOORS-1]&&engineOp==STOP&&doorsOp==OPEN)))
+
+
+##1
+
+
+// this isi the "immediatly after "
+
+
+
+    // Q8 BEGIN ******************************************************8
+
+
+
+((!(engineOp==GO && direction==UP)) throughout(
+##[0:$] ( $onehot(currentFloor)&& currentFloor[FLOORS-1] &&
+engineOp==STOP && doorsOp==OPEN)
+
+##[0:$]
+(##[0:$] (engineOp==GO && direction==DOWN)##1($onehot(currentFloor) &&engineOp==STOP && doorsOp==OPEN &&(currentFloor == $past(currentFloor >> 1)))
+)[*(FLOORS-1)]##0($onehot(currentFloor) && currentFloor[0] &&engineOp==STOP && doorsOp==OPEN)))
+ ;
+  
 endproperty
 cover_Q9: cover property (prop_Q9);
 
